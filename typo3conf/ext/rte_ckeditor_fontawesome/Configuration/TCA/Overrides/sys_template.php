03e116f5810a949fcbc2fca47fb768ef8e5ed8dc
dermohoritz <69799066+dermohoritz@users.noreply.github.com>
2021-04-07 16:17:39 +0200

    Delete typo3conf directory

--- typo3conf/ext/rte_ckeditor_fontawesome/Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,5 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die();
-
-// Add Plugin Configs
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('rte_ckeditor_fontawesome', 'Configuration/TypoScript', 'CKEditor');
--- typo3conf/ext/sms_responsive_images/Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Register base TypoScript
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'sms_responsive_images',
-    'Configuration/TypoScript/Base/',
-    'Responsive Images'
-);
